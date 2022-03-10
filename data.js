ece497ac4a0e72991ebe816be3a312af41cc8367
iambittuzz <89698917+iambittuzz@users.noreply.github.com>
2022-03-10 22:03:28 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+mongoLink:'mongodb+srv://user500:9xFx_e!mpxNbcK!@cluster0.5u03a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
+
+bot_token:'5210350968:AAGI_PFIq17ZLo_VC5eT7qwzMRRv7mExYDU',//bot token
+
+bot_name:'ShibXCoinbase_Bot',//bot username without @
+
+bot_admin: 2087872161,// admin telegram id
+
+channelsList : ['@Airdrop_crystal'],// channels list
+
+reffer_bonus:100,// refferal bonus amount
+
+min_wd:100,//min withdrawal amount
+
+daily_bonus:100,// daily bonus amount
+
+currency:'SHIB', //bot cuurency
+
+cb_api_key:'bVNzfQAazErlmVO7',//coinbase api key
+
+cb_api_secret:'86PlE38WSFBVW5EWuwrIAco65MdMWBc9',// coinbase secret key
+
+cb_account_id:'d243c4e9-268c-52c0-b70f-b8c42f88f7c9',// counbase account id for the bot currency
+
+payment_channel:'@universepayouts'// payment channel username
+
+}
